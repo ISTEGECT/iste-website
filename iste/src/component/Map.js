62e344efd021498ebe577067a88f24ce9940f31f
AkashKMathew <akashkmathew156@gmail.com>
2022-12-30 17:40:15 +0530

    form section added

--- iste/src/component/Map.js
+++ iste/src/component/Map.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import mail from '../assets/mail.svg';
+import phone from '../assets/phone.svg';
+import './Map.css'
 
 export default function Map() {
+  const [state, handleSubmit] = useForm("mwkjegaq");
+
+  const onClick = () => {
+    setTimeout(function () {
+      if (state.succeeded) {
+        alert("Message sent");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+      } else {
+        alert("Failed!!! Try again");
+      }
+    }, 1000);
+  }
+
   return (
-    <div className="Map" style={{height:'400px',marginBottom:'100px'}} id="contact">
-    <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.314923420987!2d76.22247131410091!3d10.554510792472325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7eee301ff400f%3A0x8851e3d8fc9c94f0!2sGovernment%20Engineering%20College%20-%20Thrissur%20(GECT)!5e0!3m2!1sen!2sin!4v1671185219481!5m2!1sen!2sin" width="600" height="450" style={{border:0,width:'100%'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='GECT'></iframe>
+    <div class="contMain">
+      <div className="Map" id="contact">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.314923420987!2d76.22247131410091!3d10.554510792472325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7eee301ff400f%3A0x8851e3d8fc9c94f0!2sGovernment%20Engineering%20College%20-%20Thrissur%20(GECT)!5e0!3m2!1sen!2sin!4v1671185219481!5m2!1sen!2sin" height="450" style={{ border: 0, width: '80%' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='GECT'></iframe>
+      </div>
+      <div className='formSec'>
+        <div className='formContainer'>
+          <div className='formHead'>
+            <h1>Contact us</h1>
+            <a href = "mailto:iste@gectcr.ac.in?subject=New enquiry from ISTE GECT website"><img className="contactIcon" src={mail} alt=""></img></a>
+            <a href="tel:+918139887001"><img className="contactIcon" src={phone} alt=""></img></a>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Your name
+            </label>
+            <input
+              id="name"
+              type="name"
+              name="name"
+              required
+            />
+            <label htmlFor="email">
+              Email Address
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label>
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              required
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
 
-</div>
-
+            <button type="submit" disabled={state.submitting} onClick={onClick}>
+              Send message
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
