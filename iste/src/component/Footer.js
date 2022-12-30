46ff1175c244bd11cff6e251fd8c435a0736ca5a
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-12-31 00:47:55 +0530

    icons colour changed at footer

--- iste/src/component/Footer.js
+++ iste/src/component/Footer.js
@@ -1,35 +1,32 @@
 import "./Footer.css";
-
 import React from 'react';
-
-
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-
+import {Link } from "react-scroll";
 
 const Footer = () => {
-    const style = {  width:'4vw',height:'4vw'}
+    const style = {  width:'4vw',height:'4vw',fill:'white'}
     return ( <div className="Footer">
         <div className="logofoot"><img src="./ISTE.svg" alt="" />
        </div>
        <div className="enquiry">
        <ul className="first">
-        <li>
-            <a href="/home">Home</a></li>
-            <li><a href="/events">Events</a></li>
-            <li><a href="/Contact">Contact us</a></li>
+        <li><Link to="home" spy={true} smooth={true} offset={-50} duration={500}>home</Link></li>
+
+            <li><Link to="events"spy={true} smooth={true} offset={-100} duration={500}>Events</Link></li>
+            <li><Link to="contact"spy={true} smooth={true} offset={-150} duration={500}>Contact us</Link></li>
         </ul>
         <ul className="second">
-            <li><a href="/about">About us</a></li>
-            <li><a href="/details">Objectives</a></li>
-            <li><a href="/detail">Incentives</a></li>
+            <li><Link to="about"spy={true} smooth={true} offset={-80} duration={500}>About</Link></li>
+            <li><Link to="about"spy={true} smooth={true} offset={-80} duration={500}>Objectives</Link></li>
+            
         </ul>
         
        </div>
         <div className="links">
-            <h2 className="social">SOCIAL LINKS</h2>
+            
             <div className="icons">
         <a href="Instagram"><FaInstagramSquare  style={style} className="icon"/></a>
         <a href="LinkedIn"><FaLinkedin  style={style}/></a>
