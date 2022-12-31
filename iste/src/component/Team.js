ae1880083ce56f620aff398fbd9026ef70adfff1
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-12-31 13:02:30 +0530

    ..

--- iste/src/component/Team.js
+++ iste/src/component/Team.js
@@ -6,11 +6,7 @@ import Vismaya from '../assets/VISMAYA.jpg';
 import linkedin from '../assets/linkedin.svg';
 import './Team.css';
 
-<<<<<<< HEAD:iste/src/component/TeamMembers.js
-import React from 'react';
- const profile = () => {
-   
-=======
+
 
 export default function Team() {
     const [isVisible, setVisible] = React.useState(true);
@@ -22,7 +18,7 @@ export default function Team() {
       observer.observe(domRef.current);
       return () => observer.unobserve(domRef.current);
     }, []);
->>>>>>> 385e19cd92d2b751d08ca4c4d891862a49f5e2b0:iste/src/component/Team.js
+
     return (
         
             <div className={`container fade-in-section ${isVisible ? 'is-visible' : ''}`}
@@ -102,8 +98,3 @@ export default function Team() {
     );
 };
 
-<<<<<<< HEAD:iste/src/component/TeamMembers.js
-export default profile;
-
-=======
->>>>>>> 385e19cd92d2b751d08ca4c4d891862a49f5e2b0:iste/src/component/Team.js
