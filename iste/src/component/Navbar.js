94ccbf1180d38865ae52f41b23506509d1d7784d
azmisal <azmisaleem96@gmail.com>
2022-12-31 18:49:49 +0530

    font set

--- iste/src/component/Navbar.js
+++ iste/src/component/Navbar.js
@@ -19,13 +19,12 @@ const Navbar = () => {
       <p>ISTE</p>
     </div>
     <div className={`buttons ${toggle ? "showMenu" : ""}`}>
-      <div className="but"  onClick={closeMenu}><Link to="home" spy={true} smooth={true} offset={-100} duration={500}>HOME</Link></div>
-      <div className="but"><Link to="details"spy={true} smooth={true} offset={-138} duration={500}>DETAILS</Link></div>
-
-      <div className="but"><Link to="about"spy={true} smooth={true} offset={-100} duration={500}>ABOUT</Link></div>
-      <div className="but" ><Link to="events"spy={true} smooth={true} offset={-90} duration={500}>EVENTS</Link></div>
-      <div className="but"><Link to="team"spy={true} smooth={true} offset={-100} duration={500}>EXECOM</Link></div>
-      <div className="but"><Link to="contact"spy={true} smooth={true} offset={-100} duration={500}>CONTACT</Link></div>
+      <div className="but" onClick={handleToggle}><Link to="home" spy={true} smooth={true} offset={-100} duration={500}>HOME</Link></div>
+      <div className="but" onClick={closeMenu}><Link to="details"spy={true} smooth={true} offset={-138} duration={500}>DETAILS</Link></div>
+      <div className="but" onClick={closeMenu}><Link to="about"spy={true} smooth={true} offset={-100} duration={500}>ABOUT</Link></div>
+      <div className="but" onClick={closeMenu} ><Link to="events"spy={true} smooth={true} offset={-90} duration={500}>EVENTS</Link></div>
+      <div className="but" onClick={closeMenu}><Link to="team"spy={true} smooth={true} offset={-100} duration={500}>EXECOM</Link></div>
+      <div className="but" ><Link to="contact"spy={true} smooth={true} offset={-100} duration={500}>CONTACT</Link></div>
 
 
       
